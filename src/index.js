918fc0abd02dbc213b28b5bdc5a5c221de8db17a
Jessica Soltero <jmichellevs@gmail.com>
2017-04-28 15:31:35 -0700

    finished section 2, youtube clone complete

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
-// core react library knows how to work with components, knows how to render them and nest them
-import React from 'react'
-// functionality to insert into the dom come from this library
+import _ from 'lodash'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search-bar'
+import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
 
-const API_KEY = process.env.API_KEY
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: __API_KEY__, term: term}, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
+  render() {
+
+    const videoSearch = _.debounce(term => { this.videoSearch(term) }, 300)
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => {this.setState({selectedVideo})}}
+          videos={this.state.videos}/>
+      </div>
+    )
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'))
